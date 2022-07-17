10b772bde3db6d38c5f50bbfd40042d9af4001c6
Uygar Akat <94040456+UygarAkat@users.noreply.github.com>
2022-07-17 23:14:06 +0300

    js dont work

--- javascript/animation.js
+++ javascript/animation.js
@@ -4,22 +4,17 @@ console.log(skillBars);
 function naber() {
     skillBars[0].animate(resizeAnime("23vw"), timing);
     skillBars[1].animate(resizeAnime("17vw"), timing);
-    skillBars[2].animate(resizeAnime("9vw"), timing);
+    skillBars[2].animate(resizeAnime("10vw"), timing);
     skillBars[3].animate(resizeAnime("14vw"), timing);
     skillBars[4].animate(resizeAnime("16vw"), timing);
-    setTimeout(function(){
+    setTimeout(function () {
         skillBars[0].style.width = "23vw";
         skillBars[1].style.width = "17vw";
-        skillBars[2].style.width = "9vw";
+        skillBars[2].style.width = "10vw";
         skillBars[3].style.width = "14vw";
         skillBars[4].style.width = "16vw";
     }, 1600);
 }
-
-if (saniye == 0) {
-    window.location.href = "/workingOn";
-    return;
-  }
 
 function resizeAnime(size) {
     return { width: size };
