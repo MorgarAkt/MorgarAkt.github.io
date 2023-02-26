d80739db13f2fae4f92f245b2ced7413792be795
Uygar Akat <94040456+MorgarAkt@users.noreply.github.com>
2023-02-26 23:32:07 +0300

    Old project deleted
    old project deleted

--- javascript/animation.js
+++ javascript/animation.js
@@ -3,8 +3,8 @@ console.log(skillBars);
 console.log(skillBars[0].style.width);
 
 function naber() {
-    skillBars[0].animate(resizeAnime("23vw"), timing);
-    skillBars[1].animate(resizeAnime("17vw"), timing);
+    skillBars[0].animate(resizeAnime("30vw"), timing);
+    skillBars[1].animate(resizeAnime("23vw"), timing);
     skillBars[2].animate(resizeAnime("10vw"), timing);
     skillBars[3].animate(resizeAnime("14vw"), timing);
     skillBars[4].animate(resizeAnime("16vw"), timing);
@@ -15,8 +15,8 @@ function naber() {
     skillBars[8].animate(resizeAnime("14vw"), timing);
     skillBars[9].animate(resizeAnime("16vw"), timing);
     setTimeout(function () {
-        skillBars[0].style.width = "23vw";
-        skillBars[1].style.width = "17vw";
+        skillBars[0].style.width = "30vw";
+        skillBars[1].style.width = "23vw";
         skillBars[2].style.width = "10vw";
         skillBars[3].style.width = "14vw";
         skillBars[4].style.width = "16vw";
